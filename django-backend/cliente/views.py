9e390d258a33ad5d072a70d6b9e4103ec30e4124
Freddy Mendoza <geovanny.mendoza94@gmail.com>
2023-03-13 21:45:49 -0500

    Feat: method save

--- django-backend/cliente/views.py
+++ django-backend/cliente/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from cliente.models import *
+from django.http import HttpResponse
 
 # Create your views here.
+def create(request):
+    grupo = ClienteGrupo(codigo= "000563",nombre="Empleados")
+    grupo.save()
+    return HttpResponse("Objeto creado correctamente")
+
+
+
--- django-backend/core/urls.py
+++ django-backend/core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('security/', include('security.urls')),
+    path('cliente/', include('cliente.urls')),
 ]
