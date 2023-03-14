6c4c9ab569b6bf34ac8de24dc570c0364a05a3d6
Freddy Mendoza <geovanny.mendoza94@gmail.com>
2023-03-13 22:07:48 -0500

    Feat: change ClienteAdmin

--- django-backend/inventario/admin.py
+++ django-backend/inventario/admin.py
@@ -3,4 +3,19 @@ from .models import *
 
 # Register your models here.
 admin.site.register(ProductoCategoria)
-admin.site.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = (
+        'codigo',
+        'nombre',
+        'categoria',
+        'stock_disponible',
+        'precio'
+    )
+    list_per_page = 20
+    search_fields = ('codigo','nombre')
+    list_filter = (
+        'categoria',
+        'estado'
+    )
+
+admin.site.register(Producto,ProductoAdmin)
