c561ebc4bab22fbdb485c71106162b1c49937953
Freddy Mendoza <geovanny.mendoza94@gmail.com>
2023-03-14 23:26:19 -0500

    Feat: index whit login

--- django-backend/cliente/views.py
+++ django-backend/cliente/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from cliente.models import *
 from django.http import HttpResponse
 
-<<<<<<< HEAD
+
 # Create your views here.
 def create(request):
     grupo = ClienteGrupo(codigo= "000563",nombre="Empleados")
@@ -11,5 +11,4 @@ def create(request):
 
 
 
-=======
->>>>>>> 12ec5220c181461aaa421d68e84071bb625ed608
+
